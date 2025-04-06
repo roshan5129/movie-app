a5248d0b861f8eae10e3e34751e617e4a64033c0
roshan5129 <adhikariroshan741@gmail.com>
2025-04-07 00:38:18 +0530

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = '8505fac65dd07ea95e296c8d0303f56e';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
